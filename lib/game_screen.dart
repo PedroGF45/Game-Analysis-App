91584557fae9195f579b244d76dca02c5ea3a162
Pedro Brito <pedrobfh@gmail.com>
2024-01-25 16:53:11 +0000

    Factored screens

---
+++ lib/game_screen.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+
+class GameScreen extends StatelessWidget {
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('New Screen'),
+      ),
+      body: Center(
+        child: Text('This is the new screen!'),
+      ),
+    );
+  } 
+}
